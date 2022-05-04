a8f05f9f80cfadd7c198cc4532112164c990063f
Daniel Jonathan <daniel@cosmicmind.com>
2022-05-04 15:35:23 -0600

    setup(CVERSEFOUN-3): cleanup for build and added tests

---
+++ src/utils/guard.ts
@@ -0,0 +1,51 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, Daniel Jonathan <daniel at cosmicverse dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * A guard statement. It is important to understand that
+ * guards are somewhat a reverse logic, if it fails, it
+ * returns `true`, in order to pass into a failure block. 
+ *
+ * @param {boolean} statement
+ * @returns {boolean}
+ */
+export const guard = (statement: boolean): boolean => !statement
+
+/**
+ * @template TType
+ * Checks if a value is a subtype or equal to `T`. 
+ *
+ * @param {unknown} value
+ * @param {keyof T} propertyToCheckFor
+ * @returns {boolean}
+ */
+export const guardForTypeOf = <TType>(value: unknown, propertyToCheckFor: keyof TType): value is TType => propertyToCheckFor in (value as TType)
