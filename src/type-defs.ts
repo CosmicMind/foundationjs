1b5affd09ab262e4ba1da33e51c846a8f7f03ab3
Daniel Jonathan <daniel@cosmicverse.org>
2022-10-01 20:54:17 -0500

    feat(CM-142): updated packages and cleanup

--- src/type-defs.ts
+++ src/type-defs.ts
@@ -100,4 +100,4 @@ export type PickRequired<T> = Pick<T, RequiredKeysFor<T>>
 export type PickNullable<T> = Pick<T, NullableKeysFor<T>>
 export type PickPartial<T> = Pick<T, PartialKeysFor<T>>
 
-export type UnboxPromise<T extends Promise<unknown>> = T extends Promise<infer U> ? U: never
+export type UnboxPromise<T> = T extends Promise<infer U> ? U: never
