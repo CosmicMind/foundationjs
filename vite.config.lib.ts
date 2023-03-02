f92cd905eeacc911427f7f847b3795933f85cf16
Daniel Jonathan <daniel@cosmicverse.org>
2023-03-01 21:29:17 -0600

    feat(CM-204): testing new build flow

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -38,44 +38,46 @@ import {
 import {
   defineConfig,
   LibraryFormats,
-  UserConfigExport,
 } from 'vite'
 
 import dts from 'vite-plugin-dts'
 
-const external = [
-  'json5',
-  'lib0/random.js'
-]
-
+const name = process.env.npm_package_name
 const srcDir = 'src'
-const emptyOutDir = false
+const entry = `${srcDir}/server.ts`
+const output = 'lib.es'
 const formats: LibraryFormats[] = [ 'es' ]
+const emptyOutDir = false
+const minify = 'development' !== process.env.NODE_ENV
 
-export default defineConfig(() => {
-  const minify = 'production' === process.env.NODE_ENV
-  const config: UserConfigExport = {
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL(srcDir, import.meta.url)),
-      },
+const alias = {
+  '@': fileURLToPath(new URL(srcDir, import.meta.url)),
+}
+
+const external = (id: string): boolean =>
+  !id.startsWith('/node/service') &&
+	!id.startsWith('.') &&
+	!id.startsWith('src/') &&
+	!id.startsWith('@/')
+
+export default defineConfig(() => ({
+  resolve: {
+    alias,
+  },
+  plugins: [
+    dts()
+  ],
+  build: {
+    emptyOutDir,
+    lib: {
+      name,
+      entry,
+      formats,
+      fileName: output,
     },
-    plugins: [
-      dts()
-    ],
-    build: {
-      emptyOutDir,
-      lib: {
-        name: process.env.npm_package_name,
-        entry: `${srcDir}/index.ts`,
-        formats,
-        fileName: 'lib.es',
-      },
-      rollupOptions: {
-        external,
-      },
-      minify,
+    rollupOptions: {
+      external,
     },
-  }
-  return config
-})
+    minify,
+  },
+}))
